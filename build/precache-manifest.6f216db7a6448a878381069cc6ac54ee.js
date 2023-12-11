25f7f61ad4320e0d03d08cb3a9f08cc66587050b
SaifaIIi <93119659+SaifaIIi@users.noreply.github.com>
2023-12-11 10:32:06 +0530

    added build

---
+++ build/precache-manifest.6f216db7a6448a878381069cc6ac54ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62fa51dc11df6962223b",
+    "url": "/static/css/main.5b952960.chunk.css"
+  },
+  {
+    "revision": "62fa51dc11df6962223b",
+    "url": "/static/js/main.62fa51dc.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "b0ad5db699c6abd9b29b",
+    "url": "/static/js/2.b0ad5db6.chunk.js"
+  },
+  {
+    "revision": "1458782b48af82eb3a96",
+    "url": "/static/js/runtime~main.1458782b.js"
+  },
+  {
+    "revision": "c3f7ff5da32d9877e11ba7b6fe46ea54",
+    "url": "/index.html"
+  }
+];
